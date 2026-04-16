da783a072d153ad0919fd43e14a66797b3083cc7
Ayyub22 <ayyubvalent5@gmail.com>
2026-04-16 09:19:25 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        login: resolve(__dirname, 'login.html'),
+        register: resolve(__dirname, 'register.html'),
+        fieldDetail: resolve(__dirname, 'field-detail.html'),
+        myBookings: resolve(__dirname, 'my-bookings.html'),
+        bookingDetail: resolve(__dirname, 'booking-detail.html'),
+        adminDashboard: resolve(__dirname, 'admin/dashboard.html'),
+        adminFields: resolve(__dirname, 'admin/fields.html'),
+        adminBookings: resolve(__dirname, 'admin/bookings.html'),
+        adminPayments: resolve(__dirname, 'admin/payments.html'),
+      }
+    }
+  }
 })
